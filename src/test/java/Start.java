71f785fe4d41cfd5e1d8d54123e4c6fd17ffe2f8
VovanOzz <120048518+Vovanozz@users.noreply.github.com>
2022-12-17 18:37:15 +0200

    create project

---
+++ src/test/java/Start.java
@@ -0,0 +1,35 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Start {
+
+    WebDriver wd;
+
+
+    @Test
+    public void myTest(){
+        // open browser
+        wd = new ChromeDriver();
+        wd.get("https://telranedu.web.app/");  // without history
+
+      // wd.navigate().to("https://telranedu.web.app/");
+      //  wd.navigate().back();
+       // wd.navigate().forward();
+        //wd.navigate().refresh();
+
+        wd.close();  //for one tab
+       // wd.quit();   //for all tabs
+
+    }
+
+    @Test
+    public void secondTest(){
+
+
+    }
+
+
+
+
+}
